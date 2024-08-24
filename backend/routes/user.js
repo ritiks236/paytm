2ffc057b07c007ff07fb9240a0a5a5345e16f1d1
ritiks236 <ritiks236@gmail.com>
2024-08-24 23:09:58 +0530

    Added getUsers route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -132,4 +132,32 @@ router.put("/", authMiddleware, async (req, res) => {
   });
 });
 
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
 module.express = router;
