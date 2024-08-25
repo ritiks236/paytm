61273da8e0ac3ac9c3e3aa502fa9759c5a81bb14
ritiks236 <ritiks236@gmail.com>
2024-08-25 21:32:41 +0530

    Added transaction routes

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+//backend/config.js
+module.exports = {
+  JWT_SECRET: "S3cr3t",
+};
---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const rootRouter = require("./routes/index");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", rootRouter);
+
+app.listen(3000);
