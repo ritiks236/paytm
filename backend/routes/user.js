ef8ba30078d4dad31b5b99d55b66bd6230cf9f91
ritiks236 <ritiks236@gmail.com>
2024-08-25 21:31:11 +0530

    Added account Bal

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const zod = require("zod");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
 const { authMiddleware } = require("../middleware");
@@ -41,6 +41,11 @@ router.post("/signup", async (req, res) => {
   });
 
   const userId = newUser._id;
+
+  await Account.create({
+    userId,
+    balance: 1 + Math.random() * 10000,
+  });
 
   const token = jwt.sign(
     {
@@ -160,4 +165,4 @@ router.get("/bulk", async (req, res) => {
   });
 });
 
-module.express = router;
+module.exports = router;
