6c3d8d436a1800fdfe4e3aa404675fe024855f82
ritiks236 <ritiks236@gmail.com>
2024-08-30 20:03:01 +0530

    Added components

---
+++ frontend/src/components/BottomWarning.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const BottomWarning = ({ label, buttonText, to }) => {
+  return (
+    <div className="py-2 text-sm flex justify-center">
+      <div>{label}</div>
+      <Link to={to} className="pointer underline pl-1 cursor-pointer">
+        {buttonText}
+      </Link>
+    </div>
+  );
+};
